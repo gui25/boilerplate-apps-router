4b1f825e2cc295dd765318ebcf54a7a1d1173134
Guilherme <gui25ber@gmail.com>
2023-09-15 00:57:19 -0400

    Keep calm and commit

---
+++ .lintstagedrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  // lint & Prettier TS and JS files
+  '*.{js,jsx,ts,tsx}': (filenames) => [
+    `prettier --write ${filenames.join(' ')}`,
+    `npm run lint --fix . ${filenames.join(' --file')}`
+  ]
+}
